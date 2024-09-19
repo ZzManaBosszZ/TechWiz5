d61ed479dbd48e5dcaf165ec4cf093e557a02ae0
Phuong <phuongphuong262204@gmail.com>
2024-09-19 12:41:49 +0700

    Update TripPlan.js

--- src/components/pages/TripsManagement/TripPlan.js
+++ src/components/pages/TripsManagement/TripPlan.js
@@ -14,7 +14,7 @@ function TripPlan() {
                                             <h4 class="h4-title">Destination</h4>
                                         </div>
                                         <div class="tts-description">
-                                            <a href="#">Greece</a>
+                                            <a>Greece</a>
                                         </div>
                                     </li>
                                     <li>
@@ -25,60 +25,40 @@ function TripPlan() {
                                             Lorem Ipsum
                                         </div>
                                     </li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tour-timeline-box">
+                        <div class="tt-box-number">1</div>
+                        <div class="tt-box-content">
+                            <h3 class="h3-title">Day 1: <span>Departure</span></h3>
+                            <div class="tour-timetable-schedule mt-40 mb-40">
+                                <ul>
                                     <li>
                                         <div class="tts-label">
-                                            <h4 class="h4-title">Departure Time</h4>
+                                            <h4 class="h4-title">Destination</h4>
                                         </div>
                                         <div class="tts-description">
-                                            9:15 AM To 9:30 AM.
+                                            <a>Greece</a>
                                         </div>
                                     </li>
                                     <li>
                                         <div class="tts-label">
-                                            <h4 class="h4-title">Return Time</h4>
+                                            <h4 class="h4-title">Departure</h4>
                                         </div>
                                         <div class="tts-description">
-                                            Approximately 10:30 PM.
+                                            Lorem Ipsum
                                         </div>
                                     </li>
-                                    <li>
-                                        <div class="tts-label">
-                                            <h4 class="h4-title">Dress Code</h4>
-                                        </div>
-                                        <div class="tts-description">
-                                            comfortable clothing and light jacket.
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="tts-label">
-                                            <h4 class="h4-title">Price Included</h4>
-                                        </div>
-                                        <div class="tts-description">
-                                            <ul class="included">
-                                                <li><i class="fas fa-check"></i>5 Star Accommodation</li>
-                                                <li><i class="fas fa-check"></i>Air fases</li>
-                                                <li><i class="fas fa-check"></i>3 Nights Hotel Accomodation</li>
-                                                <li><i class="fas fa-check"></i>All transportation in destination location</li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="tts-label">
-                                            <h4 class="h4-title">Price Not Included</h4>
-                                        </div>
-                                        <div class="tts-description">
-                                            <ul class="not-included">
-                                                <li><i class="fas fa-times"></i>Guide Service Fee</li>
-                                                <li><i class="fas fa-times"></i>Any Private Expenses</li>
-                                                <li><i class="fas fa-times"></i>Room Service Fees</li>
-                                            </ul>
-                                        </div>
-                                    </li>
+                                    
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
         </>
     );
